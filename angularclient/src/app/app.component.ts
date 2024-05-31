17a7c37bb2e24eb7ad6c2b8df793f002e27bf053
KellyPorter02 <porter.kelly02@gmail.com>
2024-05-31 08:32:12 -0400

    added user model and service classes

--- angularclient/src/app/app.component.ts
+++ angularclient/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularclient';
+  title = 'Spring Boot - Angular Application';
 }
